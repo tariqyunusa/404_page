83dc5e7affe49977f46c1991dcd3fe595b1e47d9
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-08-19 23:52:52 +0100

    its a wrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { LuChevronLeft } from "react-icons/lu";
 import './App.css'
 import Objects from './Matter'
+import gsap from 'gsap';
 
 function App() {
   const refresh = () => {
-    window.location.reload()
-  }
+    gsap.to([".para__btn", ".para__header", ".para__container"], {
+      y: 100,
+      opacity: 0,
+      ease: "power4.in",
+      duration: 1,
+      stagger: 0.2,
+      onComplete: () => {
+        window.location.reload(); // Reload the page after the animation completes
+      }
+    });
+  };
 
+useEffect(() => {
+  const tl = gsap.timeline()
+  tl.from([".para__container", ".para__header", ".para__btn"], {y: 70, opacity: 0, ease: "power4.in", duration: 1, stagger: 0.3})
+  
+  
+}, [])
   return (
     <>
     <section>
      <div className='container'> 
-      <p>opps! 404 Error!</p>
-      <h1>Page Not Found</h1>
-      <button onClick={refresh}><span> <LuChevronLeft /></span> Back to Home </button>
+      <p className='para__container'>opps! 404 Error!</p>
+      <h1 className='para__header'>Page Not Found</h1>
+      <button onClick={refresh} className='para__btn'><span> <LuChevronLeft /></span> Back to Home </button>
      </div>
       <Objects />
     </section>
